8d2ea4d92189f96599610ed75656a32bce75bf6b
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-18 09:14:41 +0200

    add: password generation function

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
 <?php 
 
-$arrayLetter = ["abcdefghijklmnopqrstuvwxyz"];
-$arraySpecialCharacters = ["!?&%$<>^+-*/()[]{}@#_="];
+$letters = "abcdefghijklmnopqrstuvwxyz";
+$specialCharacters = "!?&%$<>^+-*/()[]{}@#_=";
 
-$passLen = intval($_GET["passLen"]) ?? 0;
+$password = "";
 
-var_dump($passLen);
+if (isset($_GET["passLen"])){
+  $passLen = intval($_GET["passLen"]);
+  while(strlen($password) < $passLen){
+    
+    $randCase = rand(1,4);
+
+    switch ($randCase) {
+      case 1:
+        $password .= $letters[rand(0, strlen($letters) - 1)];
+        break;
+
+      case 2:
+        $password .= strtoupper($letters[rand(0, strlen($letters) - 1)]);
+        break;
+      
+      case 3:
+        $password .= $specialCharacters[rand(0, strlen($specialCharacters) - 1)];
+        break;
+
+      case 4:
+        $password .= rand(0,9);
+        break;
+      
+      default:
+        break;
+    }
+  }
+  var_dump($password);
+}
 
 ?>
 
